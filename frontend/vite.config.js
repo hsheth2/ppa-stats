740e0382f38b49fbf985e9cfc0293f2466eb3817
Harshal Sheth <hsheth2@gmail.com>
2026-06-07 18:40:33 -0700

    Upgrade Node 24 dependencies

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    cssMinify: 'esbuild',
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      '/lp-api': 'http://localhost:8000',
+    },
+  },
+});
